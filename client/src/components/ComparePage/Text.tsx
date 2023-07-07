11edad0f6f196b24bc5b4d3f097e7d0957ff6817
林玟芊 <emma@linwenqiandeMacBook-Pro.local>
2023-07-07 14:58:08 +0800

    add yml

--- client/src/components/ComparePage/Text.tsx
+++ client/src/components/ComparePage/Text.tsx
@@ -58,7 +58,9 @@ const Text = ({ branch, update }: Branch) => {
 
   return (
     <div style={{ width: '100%', borderRight: '1px solid #ececec' }}>
-      <p style={{ textAlign: 'center', borderBottom: '1px solid #ececec', paddingBottom: '16px' }}>{branch}</p>
+      <p style={{ textAlign: 'center', borderBottom: '1px solid #ececec', paddingBottom: '16px', margin: 'auto' }}>
+        {branch}
+      </p>
       <div style={{ margin: '0 24px' }}>
         <input id="title" type="text" value={title} readOnly />
         <div ref={editorRef} className="editable" contentEditable="false" />
